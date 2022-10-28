007074068f01329b0d1d4e31ca03fed1c92139f4
Niklas Schäffer <niklass.schaeffer@gmail.com>
2022-10-28 10:26:44 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   build: {
     rollupOptions: {
       output: {
