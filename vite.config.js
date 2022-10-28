92dbc2eb51f365cb5efd05d8fe3cd5bb6aa7b6ba
Niklas Schäffer <niklass.schaeffer@gmail.com>
2022-10-28 10:25:08 +0200

    chunks

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,20 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules"))
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+        },
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
