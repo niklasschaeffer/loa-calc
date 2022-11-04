8ad9640929af1262208c5be0b74120a67c6838ee
Niklas Schäffer <niklass.schaeffer@gmail.com>
2022-11-04 13:24:15 +0100

    update

--- src/components/Marie.vue
+++ src/components/Marie.vue
@@ -55,74 +55,67 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>Marie Shop Calculator</h1>
-    <br />
-    <h3>
-      Inputs
-      <span style="font-size: 14px"
-        >(Crystal Price from
-        <a href="https://www.lostarkmarket.online/">LostArk Market</a>)
-      </span>
-    </h3>
-    <div class="form-group">
-      <div class="row">
-        <div class="col">
-          <label for="inputMarketValue">Market Value</label>
-          <input
-            v-model="inputMarketValue"
-            type="number"
-            class="form-control"
-            id="inputMarketValue"
-          />
+  <form @change="calculate" @keyup="calculate">
+    <div>
+      <h1>Marie Shop Calculator</h1>
+      <br />
+      <h3>
+        Inputs
+        <span style="font-size: 14px"
+          >(Crystal Price from
+          <a href="https://www.lostarkmarket.online/">LostArk Market</a>)
+        </span>
+      </h3>
+      <div class="form-group">
+        <div class="row">
+          <div class="col">
+            <label for="inputMarketValue">Market Value</label>
+            <input
+              v-model="inputMarketValue"
+              type="number"
+              class="form-control"
+              id="inputMarketValue"
+            />
+          </div>
+          <div class="col">
+            <label for="inputMarketValue">Crystals</label>
+            <input
+              v-model="inputCrystals"
+              type="number"
+              id="inputCrystals"
+              class="form-control"
+            />
+          </div>
+          <div class="col">
+            <label for="inputCurrentGoldToCrystals">Crystal Price</label>
+            <input
+              v-model="inputCurrentGoldToCrystals"
+              type="number"
+              id="inputCurrentGoldToCrystals"
+              class="form-control"
+            />
+          </div>
         </div>
-        <div class="col">
-          <label for="inputMarketValue">Crystals</label>
-          <input
-            v-model="inputCrystals"
-            type="number"
-            id="inputCrystals"
-            class="form-control"
-          />
-        </div>
-        <div class="col">
-          <label for="inputCurrentGoldToCrystals">Crystal Price</label>
-          <input
-            v-model="inputCurrentGoldToCrystals"
-            type="number"
-            id="inputCurrentGoldToCrystals"
-            class="form-control"
-          />
+        <br />
+        <h1>Results</h1>
+        <div class="row">
+          <div class="col">
+            <div v-if="this.result > 0">
+              <p>Its worth buying on Marie's Shop.</p>
+              <p>You save a total of: {{ result }} Gold</p>
+            </div>
+            <div v-if="this.result < 0">
+              <p>Its not worth buying on Marie's Shop.</p>
+              <p>You loose a total of: {{ result }} Gold per buy.</p>
+            </div>
+            <div v-if="this.result == 0">
+              <p>Choose where you want to buy its equal in Price.</p>
+            </div>
+          </div>
         </div>
       </div>
       <br />
-      <div class="row align-items-center">
-        <div class="col">
-          <button type="button" class="btn btn-success" @click="calculate">
-            Calculate
-          </button>
-        </div>
-      </div>
-      <br />
-      <h1>Results</h1>
-      <div class="row">
-        <div class="col">
-          <div v-if="this.result > 0">
-            <p>Its worth buying on Marie's Shop.</p>
-            <p>You save a total of: {{ result }} Gold</p>
-          </div>
-          <div v-if="this.result < 0">
-            <p>Its not worth buying on Marie's Shop.</p>
-            <p>You loose a total of: {{ result }} Gold per buy.</p>
-          </div>
-          <div v-if="this.result == 0">
-            <p>Choose where you want to buy its equal in Price.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <br />
-    <!--
+      <!--
     <table class="table table-dark table-striped table-condensed">
       <thead>
         <tr>
@@ -135,8 +128,8 @@ export default {
         </tr>
       </tbody>
     </table>
-    -->
-  </div>
+    --></div>
+  </form>
 </template>
 
 <style scoped></style>
